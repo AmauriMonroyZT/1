50c49f15653d84b7a0971f8104d9544ebeded18b
Amauri Monroy <amauri_monroy@yahoo.com>
2019-02-03 23:13:43 -0600

    Version 3 con carga de video

---
+++ 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="stile.css">
+</head>
+<body>
+<?php
+require_once("encabezado.php")
+?>
+<section>
+	<article>
+		<img src="6.jpg">
+	</article>
+	<aside>
+	</aside>
+</section>
+<footer>
+	Correo: amauri_monroy@yahoo.com <br>
+	7222314654
+</footer>
+</body>
+</html>
